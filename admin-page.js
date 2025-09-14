fb9dab03416f7ea292809f88141dacd4dc9fb395
Pasha Bibko <pashabbibko@gmail.com>
2025-09-14 16:44:25 +0100

    Added a spinner whilst waiting for pages to load

---
+++ admin-page.js
@@ -0,0 +1,28 @@
+/* Default (error) page */
+const defaultPage = `
+<div id="protected-content-error">
+    <link rel="stylesheet" href="admin-page.css">
+
+    <h1 id="ProtectedContentTitle">This page is blocked</h1>
+    <p>
+        Sorry, you do not have the ability to view this page.
+        Please login
+        <a href="https://pashabibko.uk/login/login.html">here</a>.
+    </p>
+</div>`;
+
+/* Checks to see if a URL was passed as part of the URL */
+
+const query = window.location.search;
+const params = new URLSearchParams(query);
+
+const page = params.get('page');
+
+/* Gets the div to emplace the content in */
+const protectedContentDiv = document.getElementById("protected-content");
+
+/* Sets the default page if there was none provided */
+console.log(`Page to load: [${page}]`);
+if (page == null) {
+    protectedContentDiv.innerHTML = defaultPage;
+}
